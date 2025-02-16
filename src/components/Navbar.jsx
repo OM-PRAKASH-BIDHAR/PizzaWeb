279bf51eb481b2d3d5aedc9367cad9aeb56b44f5
Sakshi Yadav <125109818+SakshiY-Dev@users.noreply.github.com>
2025-02-16 12:02:20 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import { FaCartPlus } from "react-icons/fa";
 import { CiPizza } from "react-icons/ci";
-import { useCart } from "../context/CartContext"; // Import Cart Context
+import { useCart } from "../context/CartContext"; 
 
 const Navbar = () => {
-  const { cart } = useCart(); // Get cart items
+  const { cart } = useCart();
 
   return (
     <nav className="bg-red-800 h-20 px-7 text-white shadow-md text-[18px] flex items-center ">
@@ -47,7 +47,7 @@ const Navbar = () => {
             </Link>
           </li>
 
-          {/* Cart Icon with Badge */}
+          
           <li className="relative flex items-center">
             <Link
               to="/cart"
@@ -56,7 +56,7 @@ const Navbar = () => {
               <FaCartPlus />
             </Link>
 
-            {/* Badge showing cart item count */}
+        
             {cart.length > 0 && (
               <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
                 {cart.length}
