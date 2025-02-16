4013b0f43eeb481b62aecbe060b0238260115734
Sakshi Yadav <125109818+SakshiY-Dev@users.noreply.github.com>
2025-02-16 11:59:50 +0530

    Update ImageSlider.jsx

--- src/pages/ImageSlider.jsx
+++ src/pages/ImageSlider.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
-// Array of image URLs for the slider
+
 const images = [
   "https://plus.unsplash.com/premium_photo-1733259709671-9dbf22bf02cc?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1599321955726-e048426594af?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -13,13 +13,10 @@ const images = [
 ];
 
 const ImageSlider = () => {
-  // State to track the current image index
+ 
   const [currentImageIndex, setCurrentImageIndex] = useState(3);
 
-  // Function to go to the next image
-  // const nextImage = () => {
-  //   setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-  // };
+ 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
@@ -32,14 +29,14 @@ const ImageSlider = () => {
 
   return (
     <div className="relative">
-      {/* Image container */}
+     
       <img
         src={images[currentImageIndex]}
         alt="Pizza"
         className="w-full h-80 object-cover rounded-lg"
       />
 
-      {/* Left and Right Navigation Arrows */}
+     
       <button
         onClick={prevImage}
         className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white text-3xl bg-black bg-opacity-50 p-2 rounded-full"
@@ -53,7 +50,6 @@ const ImageSlider = () => {
         <FaArrowAltCircleRight />
       </button>
 
-      {/* Dot indicators */}
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {images.map((_, index) => (
           <span
